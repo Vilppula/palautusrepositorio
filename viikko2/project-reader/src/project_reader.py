abedd15e3b52e44b26a334975ac29f8619c4d36e
Lasse Vilpponen <lasse.vilpponen@gmail.com>
2023-11-09 13:35:04 +0200

    project reader lisätty

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,19 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = toml.loads(request.urlopen(self._url).read().decode("utf-8"))
+        # print(toml.loads(content))
+        poetry = content['tool']['poetry']
+        # deserialisoi TOML]-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        
+        return Project(poetry['name'], poetry['description'], poetry['license'],
+                       poetry['authors'], poetry['dependencies'], 
+                       poetry['group']['dev']['dependencies'])

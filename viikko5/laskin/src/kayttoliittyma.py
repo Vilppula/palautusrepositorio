b7c6ab7e4031afebbe5b22b042e103ff3290698f
Vilpponen Lasse M <lassevil@cubbli22-gold.local>
2023-12-04 21:54:45 +0200

    laskin

--- viikko5/laskin/src/kayttoliittyma.py
+++ viikko5/laskin/src/kayttoliittyma.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from tkinter import ttk, constants, StringVar
-
+from sovelluslogiikka import Summa, Erotus, Nollaus
 
 class Komento(Enum):
     SUMMA = 1
@@ -8,15 +8,20 @@ class Komento(Enum):
     NOLLAUS = 3
     KUMOA = 4
 
-
-class Kayttoliittyma:
+class Kayttoliittyma:   
     def __init__(self, sovelluslogiikka, root):
         self._sovelluslogiikka = sovelluslogiikka
         self._root = root
 
+        self._komennot = {
+            Komento.SUMMA: Summa(sovelluslogiikka, self._lue_syote),
+            Komento.EROTUS: Erotus(sovelluslogiikka, self._lue_syote),
+            Komento.NOLLAUS: Nollaus(sovelluslogiikka, self._lue_syote)
+        }
+
     def kaynnista(self):
         self._arvo_var = StringVar()
-        self._arvo_var.set(self._sovelluslogiikka.arvo())
+        self._arvo_var.set(self._sovelluslogiikka.hae_arvo())
         self._syote_kentta = ttk.Entry(master=self._root)
 
         tulos_teksti = ttk.Label(textvariable=self._arvo_var)
@@ -36,7 +41,7 @@ class Kayttoliittyma:
         self._nollaus_painike = ttk.Button(
             master=self._root,
             text="Nollaus",
-            state=constants.DISABLED,
+            #state=constants.DISABLED,
             command=lambda: self._suorita_komento(Komento.NOLLAUS)
         )
 
@@ -54,29 +59,19 @@ class Kayttoliittyma:
         self._nollaus_painike.grid(row=2, column=2)
         self._kumoa_painike.grid(row=2, column=3)
 
+    def _lue_syote(self):
+        return self._syote_kentta.get()
+        
+
     def _suorita_komento(self, komento):
-        arvo = 0
-
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovelluslogiikka.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovelluslogiikka.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovelluslogiikka.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
-
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
         self._kumoa_painike["state"] = constants.NORMAL
 
-        if self._sovelluslogiikka.arvo() == 0:
+        if self._sovelluslogiikka.hae_arvo() == 0:
             self._nollaus_painike["state"] = constants.DISABLED
         else:
             self._nollaus_painike["state"] = constants.NORMAL
 
         self._syote_kentta.delete(0, constants.END)
-        self._arvo_var.set(self._sovelluslogiikka.arvo())
+        self._arvo_var.set(self._sovelluslogiikka.hae_arvo())
--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -2,17 +2,39 @@ class Sovelluslogiikka:
     def __init__(self, arvo=0):
         self._arvo = arvo
 
-    def miinus(self, operandi):
-        self._arvo = self._arvo - operandi
-
-    def plus(self, operandi):
-        self._arvo = self._arvo + operandi
-
-    def nollaa(self):
-        self._arvo = 0
-
     def aseta_arvo(self, arvo):
         self._arvo = arvo
 
-    def arvo(self):
+    def hae_arvo(self):
         return self._arvo
+    
+
+class BinaariOperaatio:
+    def __init__(self, sovelluslogiikka, syote):
+        self.sovelluslogiikka = sovelluslogiikka
+        self.syote = syote
+
+    def suorita(self):
+        pass
+
+class Summa(BinaariOperaatio):
+    def __init__(self, sovelluslogiikka, syote):
+        super().__init__(sovelluslogiikka, syote)
+    
+    def suorita(self):
+        self.sovelluslogiikka.aseta_arvo(int(self.syote()) + self.sovelluslogiikka.hae_arvo())
+
+
+class Erotus(BinaariOperaatio):
+    def __init__(self, sovelluslogiikka, syote):
+        super().__init__(sovelluslogiikka, syote)
+    
+    def suorita(self):
+        self.sovelluslogiikka.aseta_arvo(self.sovelluslogiikka.hae_arvo()-int(self.syote()))
+
+class Nollaus(BinaariOperaatio):
+    def __init__(self, sovelluslogiikka, syote):
+        super().__init__(sovelluslogiikka, syote)
+    
+    def suorita(self):
+        self.sovelluslogiikka.aseta_arvo(0)
